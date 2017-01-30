bd0b72c1fbcf66f2d4f0c715016f9e72c808fc4b
maiger <mage_ege@hotmail.com>
2017-01-30 18:27:34 +0200

    Turrets now have the ability to shoot
    Turrets can now shoot bullets and destroy enemies
    Added bullets
    Added impact effect

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Turret : MonoBehaviour {
 
     private Transform target;
-    public float range = 15f;
 
+    [Header("Attributes")]
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
     public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
 	// Use this for initialization
 	void Start () {
@@ -55,7 +63,26 @@ public class Turret : MonoBehaviour {
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
 	}
+
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
+    }
 
     void OnDrawGizmosSelected()
     {
