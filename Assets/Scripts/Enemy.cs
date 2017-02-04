5f2a7d8f9778eac96d82969e9e7e8812d2e10de4
maiger <mage_ege@hotmail.com>
2017-02-04 11:44:49 +0200

    Added enemy health bars

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
@@ -7,21 +8,31 @@ public class Enemy : MonoBehaviour {
     [HideInInspector]
     public float speed;
 
-    public float health = 100;
+    public float startHealth = 100;
+    private float health;
 
     public int worth = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    // TODO: Creating a new canvas for every enemy might not be the best idea
+    // for performance reasons. Create only one canvas and use separate UI elements
+    // for every enemy and move the element position based on enemy location.
+    public Image healthBar;
+
 
     void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
 
         if(health <= 0)
         {
